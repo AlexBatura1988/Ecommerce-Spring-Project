7f910569a23a135e8a0b7c4c74dc8ec9e55eaabb
AlexBatura1988 <alex.batura88@gmail.com>
2025-04-06 23:04:30 +0300

    implemented sorting in Category class

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -4,10 +4,9 @@ import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 
-import java.util.List;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     void createCategory(Category category);
     CategoryDTO updateCategory(CategoryDTO category, Long categoryId);
     CategoryDTO deleteCategory(Long categoryId);
