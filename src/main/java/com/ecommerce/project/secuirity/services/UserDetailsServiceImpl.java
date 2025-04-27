443e6007ceb717773e6e2641d001cb3c03eb1803
AlexBatura1988 <alex.batura88@gmail.com>
2025-04-28 02:19:26 +0300

    Modified code to implement Cookie auth

--- src/main/java/com/ecommerce/project/secuirity/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/project/secuirity/services/UserDetailsServiceImpl.java
@@ -2,8 +2,6 @@ package com.ecommerce.project.secuirity.services;
 
 import com.ecommerce.project.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
